4efd91df27aebaf596731292660fcd4ada4ae5fb
E7 <liwb5@foxmail.com>
2018-05-16 21:18:20 +0800

    rewrite the DQN but it seems does not work well

--- DQN/DQN.py
+++ DQN/DQN.py
@@ -1,61 +1,255 @@
-from RL_brain import DeepQNetwork
-import gym
+import tensorflow as tf
+import pandas as pd
+import numpy as np
 
+np.random.seed(1)
+tf.set_random_seed(1)
 
+class DQN:
+    def __init__(self, 
+                n_actions, #动作空间，即有多少个动作
+                #每个状态用一个向量表示，n_features就是指向量的长度.即agent观察到的特征
+                n_features, 
+                lr = 0.01,
+                gamma = 0.9, #奖赏的折扣因子
+                epsilon = 0.1,#随机探索的概率
+                #DQN有两个network，这个参数表示每隔多少步将eval network的参数更新到target network中去。
+                replace_target_iter = 300, 
+                memory_size = 500, #记忆库容量的大小
+                batch_size = 32,
+                epsilon_decreament = None,#随着学习的进行，是否逐渐减少探索的概率
+                drop_out = 0.5,
+                ):
+        self.n_actions = n_actions
+        self.n_features = n_features
+        self.lr = lr
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.replace_target_iter = replace_target_iter
+        self.memory_size = memory_size
+        self.batch_size = batch_size
+        self.epsilon_decreament = epsilon_decreament
+        self.drop_out =drop_out
+        
+        #self.epsilon = 0 if epsilon_decreament is not None else self.epsilon
+        
+        # total learning step
+        self.learn_step_counter = 0 #用于记录学习了多少步
+        #每一行包含(s,a,r,s_)，s和s_是一个长度为n_features的向量，所以乘以2.
+        #加2则是因为r和a各占一个。
+        self.memory = np.zeros((self.memory_size, n_features * 2 + 2))
+        
+        self._build_net()
+        
+        #将eval_net的参数更新到target_net中，需要sess.run()才会执行。
+        eval_params = tf.get_collection('eval_net_params')
+        target_params = tf.get_collection('target_net_params')
+        self.replace_params_operation = [tf.assign(t, e) for t, e in zip(target_params, eval_params)]
+        
+        self.sess = tf.Session()
+        
+        self.sess.run(tf.global_variables_initializer())
+        self.cost_his = []#记录loss值，最后画出loss曲线
+    
+    def _build_net(self):
+        # ------------------ build evaluate_net ------------------#
+        # s is the input of the network
+        self.s = tf.placeholder(tf.float32, [None, self.n_features], name='s')
+        # q_target is regarded as the label of the network, 
+        self.q_target = tf.placeholder(tf.float32, [None, self.n_actions], name='q_target')
+        
+        self.drop_prob = tf.placeholder(tf.float32, name='drop_prob')
+        
+        num_L1 = 10 #神经网络第一层的输出神经元个数
+        
+        #build the network
+        with tf.variable_scope('eval_net'):
+            
+            # evaluate net的参数将会放在下面这两个集合下，第一个是自定义的。第二个是默认的
+            collections_name = ['eval_net_params', tf.GraphKeys.GLOBAL_VARIABLES]
+        
+            eval_L1 = self._add_layer(inputs = self.s, 
+                            var_scope = 'L1', 
+                            in_size = self.n_features, 
+                            out_size = num_L1, 
+                            collections_name = collections_name,
+                            acti_func = tf.nn.relu,
+                            isDropout = False)
+            
+            self.eval_L2 = self._add_layer(inputs = eval_L1, 
+                            var_scope = 'L2', 
+                            in_size = num_L1, 
+                            out_size = self.n_actions, 
+                            collections_name = collections_name,
+                            acti_func = None,
+                            isDropout = False)
+            
+        #define the loss function
+        with tf.variable_scope('loss'):
+            self.loss = tf.reduce_mean(tf.squared_difference(self.q_target, self.eval_L2))
+            
+        with tf.variable_scope('train'):
+            self._train_op = tf.train.RMSPropOptimizer(self.lr).minimize(self.loss)
+        
+        
+        # ------------------ build target_net ------------------#
+        self.s_ = tf.placeholder(tf.float32, [None, self.n_features], name='s_')
+        
+        with tf.variable_scope('target_net'):
+            
+            # evaluate net的参数将会放在下面这两个集合下，第一个是自定义的。第二个是默认的
+            collections_name = ['target_net_params', tf.GraphKeys.GLOBAL_VARIABLES]
+            
+            tar_L1 = self._add_layer(inputs = self.s_, 
+                            var_scope = 'L1', 
+                            in_size = self.n_features, 
+                            out_size = num_L1, 
+                            collections_name = collections_name,
+                            acti_func = tf.nn.relu,
+                            isDropout = False)
+            
+            self.tar_L2 = self._add_layer(inputs = tar_L1, 
+                            var_scope = 'L2', 
+                            in_size = num_L1, 
+                            out_size = self.n_actions, 
+                            collections_name = collections_name,
+                            acti_func = None,
+                            isDropout = False)
+        
+        
+    def _add_layer(self, inputs, var_scope, in_size, out_size, 
+                   collections_name=None, acti_func = None, isDropout=False):
+        
+        with tf.variable_scope(var_scope):
+            W = tf.get_variable('weights', [in_size, out_size], 
+                                initializer=tf.random_normal_initializer(0., 0.3),
+                                collections = collections_name)
+            
+            b = tf.get_variable('bias', [1, out_size],
+                                initializer = tf.constant_initializer(0.1),
+                                collections = collections_name)
+            
+            if acti_func is None:
+                y = tf.matmul(inputs, W) + b
+            else:
+                y = acti_func(tf.matmul(inputs, W) + b)
+        
+            if isDropout:
+                y = tf.nn.dropout(y, keep_prob=self.drop_prob)
+            return y
+            
+        
+        
+    def store_transition(self, s, a, r, s_):
+        """保存一次动作后的当前状态s，动作a，奖赏r，下一个状态s_
+        """
+        if not hasattr(self, 'memory_counter'):
+            self.memory_counter = 0
+        
+        transition = np.hstack((s, [a, r], s_))
+        index = self.memory_counter % self.memory_size
+        self.memory[index, :] = transition
+        self.memory_counter += 1
+        
+    def choose_action(self, obsevation):
+        """输入一个obsevation（s），让神经网络输出一个action。
+        以前没有神经网络的时候是直接找Q值最大对应的action，
+        现在的Q值是由神经网络产生的。
+        
+        obsevation作为eval_net的输入，eval_net是用来预测的。target_net是作为label的。
+        
+        return：
+            action：int型的数据
+        """
+        obsevation = obsevation[np.newaxis, :]
+        
+        if np.random.uniform() > self.epsilon:
+            actions_value = self.sess.run(self.eval_L2, 
+                                         feed_dict={self.s: obsevation})#, 
+                                                    #self.drop_prob: self.drop_out})
+                
+            action = np.argmax(actions_value)#选择q值最大对应的action
+            
+        else:
+            action = np.random.randint(0, self.n_actions)
+            
+        return action
+    
+    
+    def learn(self):
+        """"""
+        #检查是否要将eval_net的参数更新到target_net
+        if self.learn_step_counter % self.replace_target_iter == 0:
+            self.sess.run(self.replace_params_operation)
+            print('\n target_params_replaced \n')
+            
+        #说明记忆库已经填满了，可以随机从中采样
+        if self.memory_counter > self.memory_size:
+            sample_index = np.random.choice(self.memory_size, size=self.batch_size)
+            
+        else:
+            sample_index = np.random.choice(self.memory_counter, size = self.batch_size)
+        
+        batch_memory = self.memory[sample_index, :]
+        
+        feed_dict = {self.s_: batch_memory[:,-self.n_features:],
+                     self.s: batch_memory[:, :self.n_features]}
+        #我们要把q_next当成是最优的，利用它来更新q_target
+        q_next, q_eval = self.sess.run([self.tar_L2, self.eval_L2],
+                                         feed_dict = feed_dict)
+        
+        q_target = q_eval.copy()#因为选择动作的时候只会选择一个，所以要保证其他的动作不会影响到loss
+        batch_index = np.arange(self.batch_size, dtype=np.int32)
+        eval_act_index = batch_memory[:, self.n_features].astype(int)
+        reward = batch_memory[:, self.n_features + 1]
+        q_target[batch_index, eval_act_index] = reward + self.gamma * np.max(q_next, axis=1)
+        
+        """
+        For example in this batch I have 2 samples and 3 actions:
+        q_eval =
+        [[1, 2, 3],
+         [4, 5, 6]]
 
-env = gym.make('CartPole-v0')
-env = env.unwrapped
-#
-# print(env.action_space) # 查看这个环境中可用的 action 有多少个
-# print(env.observation_space)    # 查看这个环境中可用的 state 的 observation 有多少个
-# print(env.observation_space.high)   # 查看 observation 最高取值
-# print(env.observation_space.low)    # 查看 observation 最低取值
+        q_target = q_eval =
+        [[1, 2, 3],
+         [4, 5, 6]]
 
-RL = DeepQNetwork(n_actions=env.action_space.n,
-                  n_features=env.observation_space.shape[0],
-                  learning_rate=0.01,
-                  e_greedy=0.9,
-                  replace_target_iter=100,
-                  memory_size=2000,
-                  e_greedy_increment=0.0008,
-                  )
+        Then change q_target with the real q_target value w.r.t the q_eval's action.
+        For example in:
+            sample 0, I took action 0, and the max q_target value is -1;
+            sample 1, I took action 2, and the max q_target value is -2:
+        q_target =
+        [[-1, 2, 3],
+         [4, 5, -2]]
 
-total_step = 0  #record step number
+        So the (q_target - q_eval) becomes:
+        [[(-1)-(1), 0, 0],
+         [0, 0, (-2)-(6)]]
 
-for i_episode in range(100):
-    observation = env.reset()
-    ep_r = 0
-
-    while True:
-        env.render()  #刷新环境
-
-        action = RL.choose_action(observation)
-
-        observation_, reward, done, info = env.step(action)
-
-        # x 是车的水平位移, 所以 r1 是车越偏离中心, 分越少
-        # theta 是棒子离垂直的角度, 角度越大, 越不垂直. 所以 r2 是棒越垂直, 分越高
-        x, x_dot, theta, theta_dot = observation_
-        r1 = (env.x_threshold - abs(x))/env.x_threshold - 0.8
-        r2 = (env.theta_threshold_radians - abs(theta))/env.theta_threshold_radians -0.5
-
-        reward = r1 + r2
-
-        RL.store_transition(observation, action, reward, observation_)
-
-        if total_step > 1000:
-            RL.learn()
-
-        ep_r += reward
-        if done:
-            print('episode:',i_episode,
-                  'ep_r:', round(ep_r, 2),
-                  'epsilon:', round(RL.epsilon,2)
-                  )
-            break
-
-        observation = observation_
-        total_step += 1
-
-
-RL.plot_cost()
+        We then backpropagate this error w.r.t the corresponding action to network,
+        leave other action as error=0 cause we didn't choose it.
+        """        
+        
+        #经过上面的计算q_target才被认为是label
+        #接下来再进行训练
+        feed_dict = {self.s: batch_memory[:, :self.n_features],
+                     self.q_target: q_target}
+        _, self.cost = self.sess.run([self._train_op, self.loss],
+                                     feed_dict = feed_dict)
+        
+        #保存loss值，方便画图
+        self.cost_his.append(self.cost)
+        
+        self.learn_step_counter += 1
+        
+        #这里需要考虑epsilon随时间变小
+        if self.epsilon > 0.05:
+            self.epsilon = self.epsilon - self.epsilon_decreament
+        
+    def plot_cost(self):
+        import matplotlib.pyplot as plt
+        plt.plot(np.arange(len(self.cost_his)), self.cost_his)
+        plt.ylabel('Cost')
+        plt.xlabel('training steps')
+        plt.show()        
+        
---
+++ DQN/main.py
@@ -0,0 +1,124 @@
+from DQN import DQN
+import gym
+import argparse
+import sys
+
+def CartPole_v0(args):
+    env = gym.make('CartPole-v0')
+    env = env.unwrapped
+    #
+    # print(env.action_space) # 查看这个环境中可用的 action 有多少个
+    # print(env.observation_space)    # 查看这个环境中可用的 state 的 observation 有多少个
+    # print(env.observation_space.high)   # 查看 observation 最高取值
+    # print(env.observation_space.low)    # 查看 observation 最低取值
+
+    agent = DQN(n_actions = env.action_space.n, 
+                n_features = env.observation_space.shape[0],
+                lr = args.lr,
+                gamma = args.gamma, 
+                epsilon = args.epsilon,
+                replace_target_iter = args.update_when,
+                memory_size = args.memory_size,
+                batch_size = args.batch_size,
+                epsilon_decreament = 0.000008,
+                drop_out = args.drop_out)
+
+    total_step = 0
+    for epoch in range(1, args.epoches+1):
+        observation = env.reset()
+        reward_every_epoch = 0
+
+        while True:
+            env.render() #刷新环境
+
+            action = agent.choose_action(observation)
+
+            observation_,reward, done, info = env.step(action)
+
+            # x: 小车的水平位置, x==0就是在中间位置。
+            # theta: 棒与垂直地面的线的夹角，角度越大，棒就越倾向地面，越不稳定。 
+            x, x_dot, theta, theta_dot = observation_
+            r1 = (env.x_threshold - abs(x))/env.x_threshold - 0.5
+            r2 = (env.theta_threshold_radians - abs(x))/env.theta_threshold_radians - 0.8
+
+            reward = r1 + r2
+
+            agent.store_transition(observation, action, reward, observation_)
+
+            if total_step > 1000:
+                agent.learn()
+
+            reward_every_epoch += reward
+            if done:
+                print('episode: ', epoch,
+                      '| reward_every_epoch: ', round(reward_every_epoch, 2),
+                      '| epsilon: ', round(agent.epsilon,2))
+                break
+
+            observation = observation_
+            total_step += 1
+            
+            
+    agent.plot_cost()
+    
+    
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.register('type', 'bool', lambda v:v.lower() == 'true')
+    
+    parser.add_argument(
+        '--memory_size',
+        type = int,
+        default = 2000,
+        help='the size of the memory of the agent.(default = 2000)')
+    
+    parser.add_argument(
+        '--gamma',
+        type = float,
+        default = 0.9, 
+        help='the discount factor of the reward.(default = 0.9)')
+    
+    parser.add_argument(
+        '--lr',
+        type = float,
+        default = 0.01,
+        help = 'the learning rate of the model(default = 0.001)')
+    
+    parser.add_argument(
+        '--epoches',
+        type = int,
+        default = 1000,
+        help = 'the max number of epoch(default = 1000)')
+    
+    parser.add_argument(
+        '--batch_size',
+        type = int,
+        default = 32,
+        help = 'the batch size of train data(default = 128)')
+    
+    parser.add_argument(
+        '--drop_out',
+        type = float,
+        default = 0.5,
+        help = 'the probability of drop out(default = 0.5) ')
+
+    parser.add_argument(
+        '--epsilon',
+        type = float,
+        default = 0.1,
+        help = 'the e_greedy probability.(default = 0.1) ')
+    
+    #每隔多少步就将eval net的参数更新到target net中
+    parser.add_argument(
+        '--update_when',
+        type = int,
+        default = 100,
+        help = 'update the params in eval net to target net.(default = 500)')
+    
+    args, unparsed = parser.parse_known_args()  
+    print(args)
+
+    CartPole_v0(args)
+
+
